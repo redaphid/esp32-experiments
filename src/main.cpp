f9089abd8f44939bb8c77618a585c40541fe21ef
redaphid <iam@aaronherres.com>
2022-12-01 14:46:22 -0700

    debugging hello world serial out

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.print("hi from setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World!");
-  delay(1000);
+  delay(100);
 }
