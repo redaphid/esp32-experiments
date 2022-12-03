5d82ddb2fca75d373a8a67e3cbe724c855e4a0c2
redaphid <iam@aaronherres.com>
2022-12-03 02:11:29 -0700

    aaaaannnd we have a 'Hello World!'

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
 #include <Arduino.h>
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  delay(1000);
-  
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+#ifndef LED_BUILTIN
+    #define LED_BUILTIN 2
+#endif
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");
-  // Serial.flush();
-  delay(100);
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(1000);
 }
