bec26ce3c889c2465596c9df2f773010ff4ce771
redaphid <iam@aaronherres.com>
2022-12-04 17:13:25 -0700

    omg lights blink

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,22 @@
 #define J11 23
 void setup()
 {
-
-  pinMode(J9, OUTPUT);
+  pinMode(J9, INPUT);
   pinMode(J11, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(J9, LOW);
 
-  digitalWrite(J11, LOW);
-  delay(10);
-  digitalWrite(J11, HIGH);
-  digitalWrite(J9, HIGH);
-  delay(50);
+  if (digitalRead(J9) == HIGH)
+  {
+    delay(100);
+    digitalWrite(J11, HIGH);
+    delay(100);
+    digitalWrite(J11, LOW);
+  }
+  else
+  {
+    digitalWrite(J11, LOW);
+  }
 }
