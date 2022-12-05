d869d8b0ea12eba1caee920369018e4cf901e1c1
redaphid <iam@aaronherres.com>
2022-12-04 17:10:21 -0700

    J11 BLINKS OMG

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #define J9 22
-
+#define J11 23
 void setup()
 {
 
   pinMode(J9, OUTPUT);
+  pinMode(J11, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(J9, LOW);
-  delay(100);
+
+  digitalWrite(J11, LOW);
+  delay(10);
+  digitalWrite(J11, HIGH);
   digitalWrite(J9, HIGH);
-  delay(100);
+  delay(50);
 }
