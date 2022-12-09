373bf2445fe92d9179cf97783d54e30cb883e538
redaphid <iam@aaronherres.com>
2022-12-08 21:01:27 -1000

    oled works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include "heltec.h"
 #define J9 22
 #define J11 23
 
 #define SOFT_LED_CHECK 23
-#define NUM_LEDS 30
+#define NUM_LEDS 10
 
 CRGB leds[NUM_LEDS];
 #define DATA_PIN 22
@@ -14,6 +15,8 @@ void setup()
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SOFT_LED_CHECK, OUTPUT);
+  Heltec.begin(true /* DisplayEnable Enable */, false /* LoRa Disable */, true /* Serial Enable */);
+  Heltec.display->setContrast(255);
 }
 
 void loop()
@@ -23,7 +26,7 @@ void loop()
   // {
   static uint8_t hue = 0;
   FastLED.showColor(CHSV(hue++, 255, 255));
-  delay(10);
+  delay(100);
   // }
   // else
   // {
