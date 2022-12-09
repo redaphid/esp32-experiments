17525781336aa4db5236851f342957eefa613081
redaphid <iam@aaronherres.com>
2022-12-09 00:39:28 -1000

    distance functions for rssid

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 #include <FastLED.h>
 #include <BLEDevice.h>
 #include "heltec.h"
+// using namespace std;
+
 #define J9 22
 #define J11 23
 
-#define SOFT_LED_CHECK 23
 #define NUM_LEDS 20
 
 CRGB leds[NUM_LEDS];
@@ -17,23 +18,52 @@ BLEScan *pBLEScan;
 
 int maxRssi = -70;
 int strength = 0;
+double distance = 0;
+std::string name = "";
 static uint8_t hue = 0;
+
+double rssiToDistance(int rssi)
+{
+  int txPower = -59; // hard coded power value. Usually ranges between -59 to -65
+
+  if (rssi == 0)
+  {
+    return -1.0; // if we cannot determine accuracy, return -1.
+  }
+
+  double ratio = rssi * 1.0 / txPower;
+  if (ratio < 1.0)
+  {
+    return pow(ratio, 10);
+  }
+  else
+  {
+    double accuracy = (0.89976) * pow(ratio, 7.7095) + 0.111;
+    return accuracy;
+  }
+}
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
   void onResult(BLEAdvertisedDevice advertisedDevice)
   {
-    auto rssi = advertisedDevice.getRSSI();
-    maxRssi = max(maxRssi, rssi);
+    name = advertisedDevice.getName();
+    if (name[0] == 'M')
+    {
+      auto rssi = advertisedDevice.getRSSI();
+      maxRssi = max(maxRssi, rssi);
+    }
   }
 };
 
 void EndScanCallback(BLEScanResults results)
 {
-  pBLEScan->clearResults();
-  strength = map(maxRssi, -100, -60, 0, 100);
+  distance = rssiToDistance(maxRssi);
+  if (distance > 50) distance = 50;
+
+  strength = map(distance * -1, -50, -1, 0, 100);
   maxRssi = -100;
-  pBLEScan->start(5, EndScanCallback);
+  pBLEScan->start(5, EndScanCallback, false);
 }
 void scanForBleDevices()
 {
@@ -42,7 +72,7 @@ void scanForBleDevices()
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->start(5, EndScanCallback);
+  pBLEScan->start(5, EndScanCallback, false);
 }
 
 void setup()
@@ -64,15 +94,16 @@ void loop()
 
   int ledsToLight = map(strength, 0, 100, 0, NUM_LEDS);
   int progress = strength;
+
+  hue = map(strength, 0,100,0, 255 - (NUM_LEDS*5));
   oled->clear();
-  oled->drawString(0, 0, "RSSI: " + String(maxRssi));
+  oled->drawString(0, 0, String(distance));
   oled->drawProgressBar(0, 20, 128, 10, progress);
   for (int i = 0; i < NUM_LEDS; i++)
   {
     if (i < ledsToLight)
     {
-      hue = (hue + 1) % 255;
-      leds[i] = CHSV(hue, 255, 255);
+      leds[i] = CHSV(hue+=5, 255, 255);
     }
     else
     {
